9d4294b981de86e1527dc5d53caaee0b631bbfaa
Your Name <yogesh.nandakumar@gmail.com>
2016-09-11 18:51:07 -0400

    fix(module-api): allow case insensitive JSON mapping in controllers

--- server/src/main/java/xyz/yogesh/app/AppConfig.java
+++ server/src/main/java/xyz/yogesh/app/AppConfig.java
@@ -1,12 +1,32 @@
 package xyz.yogesh.app;
 
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 @ComponentScan
 @EnableWebMvc
-public class AppConfig {
-
+public class AppConfig extends WebMvcConfigurerAdapter {
+	@Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(converter());
+        }
+    
+    @Bean
+    public MappingJackson2HttpMessageConverter converter() {
+        ObjectMapper om = new ObjectMapper();
+        om.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+        MappingJackson2HttpMessageConverter mc = new MappingJackson2HttpMessageConverter(om);
+        return mc;
+    }
 }
