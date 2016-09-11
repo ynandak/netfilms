9d3c701042eba266ab85326c3ade5eb8e8f56ce1
Your Name <yogesh.nandakumar@gmail.com>
2016-09-11 04:59:41 -0400

    feat(module-api): add Review entity and handlers

---
+++ server/src/main/java/xyz/yogesh/app/entity/Review.java
@@ -0,0 +1,80 @@
+package xyz.yogesh.app.entity;
+
+import java.util.UUID;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table
+@NamedQueries({
+		@NamedQuery(name = "Review.findAll", query = "SELECT r from Review r where r.title.id=:pVidID"),
+		@NamedQuery(name = "Review.findByTitleUser", query = "SELECT r from Review r where r.title.id=:pVidID and r.user.id=:pUserID")
+})
+public class Review {
+
+	@Id
+	private String id;
+
+	@OneToOne
+	private User user;
+	
+	@OneToOne
+	private VideoRelease title;
+	private Integer rating;
+	private String review;
+
+	public Review() {
+		id = UUID.randomUUID().toString();
+	}
+
+	@Override
+	public String toString() {
+		return "Review [id=" + id + ", user=" + user + ", title=" + title + ", rating=" + rating + ", review=" + review
+				+ "]";
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public VideoRelease getTitle() {
+		return title;
+	}
+
+	public void setTitle(VideoRelease title) {
+		this.title = title;
+	}
+
+	public Integer getRating() {
+		return rating;
+	}
+
+	public void setRating(Integer rating) {
+		this.rating = rating;
+	}
+
+	public String getReview() {
+		return review;
+	}
+
+	public void setReview(String review) {
+		this.review = review;
+	}
+}
---
+++ server/src/main/java/xyz/yogesh/app/repository/ReviewRepository.java
@@ -0,0 +1,39 @@
+package xyz.yogesh.app.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import org.springframework.stereotype.Repository;
+
+import xyz.yogesh.app.entity.Review;
+
+@Repository
+public class ReviewRepository{
+
+	@PersistenceContext
+	private EntityManager em;
+
+	public List<Review> findAll(String vidID) {
+		TypedQuery<Review> query = em.createNamedQuery("Review.findAll", Review.class);
+		query.setParameter("pVidID", vidID);
+		return query.getResultList();
+	}
+	
+	public Review create(Review review) {
+		em.persist(review);
+		return review;
+	}
+
+	public boolean hasTitleUser(String vidID, String userID) {
+		TypedQuery<Review> query = em.createNamedQuery("Review.findByTitleUser", Review.class);
+		query.setParameter("pVidID", vidID);
+		query.setParameter("pUserID", userID);
+		if(query.getResultList().size()==0) {
+			return false;
+		}
+		return true;
+	}
+}
